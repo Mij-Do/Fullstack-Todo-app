59cd07837b111d2f539cd37513739bb07d340089
ahmed <ahmedmijdo2@gmail.com>
2025-08-18 06:57:29 +0300

    build my own paginator

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import Paginator from "../components/ui/Paginator";
 import { customQueryHook } from "../hooks";
 import type { ITodo } from "../interfaces";
 
-const Todos = () => {
+const TodosPage = () => {
     const storageKey = "loggedInUser";
     const userDataString = localStorage.getItem(storageKey);
     const userData = userDataString ? JSON.parse(userDataString) : null;
 
-    const {isLoading, data} = customQueryHook({
-            queryKey: ["paginated todos"],
-            url: "/users/me?populate=todos",
+    // states 
+    const [page, setPage] = useState<number>(1);
+
+    // handellers
+    const onClickPrev = () => {
+        setPage(prev => prev - 1);
+    }
+    const onClickNext = () => {
+        setPage(prev => prev + 1);
+    }
+
+    const {isLoading, isFetching, data} = customQueryHook({
+            queryKey: [`todos-page-${page}`],
+            url: "/todos?pagination[pageSize]=10&pagination[page]=1",
             config: {
                 headers: {
                     Authorization: `Bearer ${userData.jwt}`
@@ -26,7 +38,7 @@ const Todos = () => {
         
     return (
         <div>
-            {data.todos.length ? data.todos.map((todo: ITodo) => (
+            {data.data.length ? data.data.map((todo: ITodo) => (
                 <div key={todo.id} className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
                     <p className="w-full font-semibold">
                         {todo.id}- {todo.title}
@@ -34,10 +46,17 @@ const Todos = () => {
                 </div>
             )) : <h3> No Todos yet! </h3>}
 
-            <Paginator />
+            <Paginator 
+                page={page} 
+                pageCount={data.meta.pagination.pageCount} 
+                total={data.meta.pagination.total} 
+                isLoading={isLoading || isFetching} 
+                onClickPrev={onClickPrev} 
+                onClickNext={onClickNext}
+            />
         </div>
 
     )
 }
 
-export default Todos;
+export default TodosPage;
